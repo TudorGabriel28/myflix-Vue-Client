23b7cd2f47153addb90bb03ad970aa942302e026
Tudor Gabriel <tudordangabriel28@gmail.com>
2022-03-27 21:19:42 +0300

    Create Vue CLI project.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,12 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/airbnb',
+    'prettier'
   ],
   parserOptions: {
     parser: '@babel/eslint-parser',
   },
+  plugins: ['prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,8 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: 'https://localhost:7129',
+    port: 7129,
+  },
 });
