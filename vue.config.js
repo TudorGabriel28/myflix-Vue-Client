4f20fee6e8490d7f8582d91f4da0443fb5c3635c
Tudor Gabriel <tudordangabriel28@gmail.com>
2022-04-04 23:18:35 +0300

    Remove unnecessary config options.

--- vue.config.js
+++ vue.config.js
@@ -8,15 +8,6 @@ module.exports = {
       filename: 'index.html'
     }
   },
-  outputDir: 'build',
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src')
-      },
-      extensions: ['.js', '.vue', '.json']
-    }
-  },
   devServer: {
     proxy: 'https://localhost:7129',
     port: 7129
