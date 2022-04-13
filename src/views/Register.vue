8b47e555626bee0a334ce53f3eea772e1f2edf79
Tudor Gabriel <tudordangabriel28@gmail.com>
2022-04-13 23:48:19 +0300

    Remove register form padding on mobile.

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,55 +1,54 @@
 <template lang="pug">
-.row.align-items-center
-  .center-container.col-10.offset-1.col-sm-8.offset-sm-2.col-md-6.offset-md-3.col-xl-4.offset-xl-4
-    .form-container.px-5.py-5
-      h3.h3.title(data-test-id='title') Sign Up
-      form(@submit.prevent='registerUser')
-        input.input(
-          type='text',
-          placeholder='First Name',
-          v-model='firstname',
-          data-test-id='register-first-name-input'
-        )
-        span.red(data-test-id='register-first-name-error') {{ firstNameError }}
-        input.input(
-          type='text',
-          placeholder='Last Name',
-          v-model='lastname',
-          data-test-id='register-last-name-input'
-        )
-        span.red(data-test-id='register-last-name-error') {{ lastNameError }}
-        input.input(
-          type='text',
-          placeholder='Nickname',
-          v-model='nickname',
-          data-test-id='register-nickname-input'
-        )
-        input.input(
-          type='text',
-          placeholder='Email address',
-          v-model='email',
-          data-test-id='register-email-input'
-        )
-        span.red(data-test-id='register-email-error') {{ emailError }}
-        input.input(
-          type='password',
-          placeholder='Password',
-          v-model='password',
-          data-test-id='register-password-input'
-        )
-        span.red(data-test-id='register-password-error') {{ passwordError }}
-        input.input(
-          type='password',
-          placeholder='Confirm password',
-          v-model='confirmPassword',
-          data-test-id='register-confirm-password-input'
-        )
-        span.red(data-test-id='register-confirm-password-error') {{ confirmPasswordError }}
-        .pt-5
-          button.button(type='submit') Sign Up
-      .form-message
-        p Already signed up? Use your email and password to
-          router-link(:to='{ name: "Login" }') log in
+.center-container.col-sm-8.offset-sm-2.col-md-6.offset-md-3.col-xl-4.offset-xl-4.col-xxl-3.offset-xxl-4
+  .form-container.px-5.py-5
+    h3.h3.title(data-test-id='title') Sign Up
+    form(@submit.prevent='registerUser')
+      input.input(
+        type='text',
+        placeholder='First Name',
+        v-model='firstname',
+        data-test-id='register-first-name-input'
+      )
+      span.red(data-test-id='register-first-name-error') {{ firstNameError }}
+      input.input(
+        type='text',
+        placeholder='Last Name',
+        v-model='lastname',
+        data-test-id='register-last-name-input'
+      )
+      span.red(data-test-id='register-last-name-error') {{ lastNameError }}
+      input.input(
+        type='text',
+        placeholder='Nickname',
+        v-model='nickname',
+        data-test-id='register-nickname-input'
+      )
+      input.input(
+        type='text',
+        placeholder='Email address',
+        v-model='email',
+        data-test-id='register-email-input'
+      )
+      span.red(data-test-id='register-email-error') {{ emailError }}
+      input.input(
+        type='password',
+        placeholder='Password',
+        v-model='password',
+        data-test-id='register-password-input'
+      )
+      span.red(data-test-id='register-password-error') {{ passwordError }}
+      input.input(
+        type='password',
+        placeholder='Confirm password',
+        v-model='confirmPassword',
+        data-test-id='register-confirm-password-input'
+      )
+      span.red(data-test-id='register-confirm-password-error') {{ confirmPasswordError }}
+      .pt-5
+        button.button(type='submit') Sign Up
+    .form-message
+      p Already signed up? Use your email and password to
+        router-link(:to='{ name: "Login" }') log in
 </template>
 
 <script>
